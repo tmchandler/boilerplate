39be6ae17ddec9781f1ba8b6d35a71440ec9b7ac
Steve <steve.gackstetter@gmail.com>
2021-02-22 16:21:54 -0600

    2/22 workshop progress

--- server/index.js
+++ server/index.js
@@ -1,1 +1,29 @@
 // server entry file
+const express = require('express');
+const donuts = require('./db/index.js');
+
+var app = express();
+
+// express static
+const port = 3000;
+
+app.get('/donuts', (req, res) => {
+  // get all the donos
+  console.log('hello from /get');
+  donuts.getAllDonuts((err, donuts) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      console.log("🚀 ~ file: index.js ~ line 18 ~ donuts.getAllDonuts ~ donuts", donuts)
+      res.send(donuts);
+    }
+  })
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    throw err;
+  } else {
+    console.log('server running on ' + port);
+  }
+})
