bdd861c4ea3196aac7e1a51425fb0483743fffa7
tmchandler <chandlertom71@gmail.com>
2021-09-01 17:10:51 -0500

    progress from workshop

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,17 @@
 -- Write a database schema! --
+DROP DATABASE IF EXISTS nasa;
 
+CREATE DATABASE nasa;
+
+USE nasa;
+
+CREATE TABLE photos (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  image_url VARCHAR(500) NOT NULL,
+  title VARCHAR(200),
+  description VARCHAR(2000),
+  date VARCHAR(20)
+);
 
 
 
